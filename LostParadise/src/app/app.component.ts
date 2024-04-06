2d7111aa75e53b9bcee3bf323fabcd296353f10c
Edward Finke <finkeedward@gmail.com>
2024-04-06 20:19:59 +0200

    more stuff on contact and other small imporvments

--- LostParadise/src/app/app.component.ts
+++ LostParadise/src/app/app.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+
 
 @Component({
   selector: 'app-root',
@@ -9,7 +10,10 @@ import { FooterComponent } from './footer/footer.component';
   imports: [RouterOutlet, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
+  
 })
 export class AppComponent {
   title = 'LostParadise';
+  
 }
